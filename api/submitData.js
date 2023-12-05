0815b95f58564d968d44efd441d68b8f3d539885
caddoSmith229 <caddo.caleb229@gmail.com>
2023-12-05 02:07:12 -0600

    fixed

--- api/submitData.js
+++ api/submitData.js
@@ -2,6 +2,7 @@ module.exports = async (req, res) => {
     const fetch = require('node-fetch');
 
     const googleScriptURL = 'https://script.google.com/macros/s/AKfycby2cmOP3yphJd_qJkGgM2-jnHSmU6NxlSPqM5H8_5fHrUCKdMjorXCAPTXqEOX6Lwc2dA/exec';
+    console.log(googleScriptURL);
 
     try {
         const response = await fetch(googleScriptURL, {
@@ -11,8 +12,10 @@ module.exports = async (req, res) => {
         });
 
         const data = await response.json();
+        console.log("data:", data)
         res.status(200).json(data);
     } catch (error) {
+        console.log("error:", error)
         res.status(500).json({ error: error.message });
     }
 };
