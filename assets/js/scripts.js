2f3f4751c27e03cfc80a542ae497a95873e8f69e
caddoSmith229 <caddo.caleb229@gmail.com>
2023-12-05 06:39:47 -0600

    added the background audio

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,7 @@
-
+var API_KEY = 'AIzaSyCbjmWnDygEhRKO8khbE6n1BowwvW_YMiA';
+var SPREADSHEET_ID = '1IEg9T_HyTGlnxp4zBs9fiZWAMhzI3pGA70TAYDxHCgg';
+var SHEET_NAME = 'Sheet1';
+const CLIENT_ID = '657416883280-7h3g43eftigjdq7lebjp7e6lrvcbvft2.apps.googleusercontent.com';
 
 $(document).ready(function () {
     setTimeout(function () {
@@ -55,26 +58,101 @@ $(document).ready(function () {
 
     $('form').on('submit', function (e) {
         e.preventDefault();
+        gapi.load('client:auth2', initClient);
+    });
+
+    function initClient() {
+        gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+            scope: "https://www.googleapis.com/auth/spreadsheets",
+        }).then(function () {
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+            // Handle the initial sign-in state.
+            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        });
+    }
+
+    function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+            // User is signed in.
+            submitForm();
+        } else {
+            // User is not signed in. Start Google OAuth flow.
+            gapi.auth2.getAuthInstance().signIn();
+        }
+    }
+
+    function submitForm() {
         var canvas = document.getElementById('canvas');
         var imageDataUrl = canvas.toDataURL('image/png'); // or 'image/jpeg'
         var formData = {
-            email: $('#email').val(),
-            twitter: $('#twitterHandle').val(),
-            image: imageDataUrl
+            'email': $('#email').val(),
+            'twitter': $('#twitterHandle').val(),
+            'image': imageDataUrl
         };
 
-        $.ajax({
-            type: 'POST',
-            url: 'https://agoric-ugly-sweater.vercel.app/api/submitData',
-            contentType: 'application/json',
-            data: JSON.stringify(formData),
-            success: function (response) {
-                console.log(response);
+        gapi.client.sheets.spreadsheets.values.append({
+            spreadsheetId: SPREADSHEET_ID,
+            range: `${SHEET_NAME}!A1`,
+            valueInputOption: "USER_ENTERED",
+            resource: { formData },
+        }).then(
+            function (response) {
+                console.log('Form submitted successfully', response);
             },
-            error: function (err) {
-                console.log(err);
+            function (error) {
+                console.error('Error submitting form', error);
             }
-        });
-    });
+        );
+    }
+
+    // $('form').on('submit', function (e) {
+    //     e.preventDefault();
+    //     var canvas = document.getElementById('canvas');
+    //     var imageDataUrl = canvas.toDataURL('image/png'); // or 'image/jpeg'
+    //     var formData = {
+    //         'email': $('#email').val(),
+    //         'twitter': $('#twitterHandle').val(),
+    //         'image': imageDataUrl
+    //     };
+
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SHEET_NAME}!A1:append`,
+    //         contentType: 'application/json',
+    //         headers: {
+    //             'Authorization': `Bearer ${YOUR_API_KEY}`,
+    //             'Content-Type': 'application/json'
+    //         },
+    //         data: JSON.stringify({
+    //             "values": [
+    //                 [formData.email, formData.twitter, formData.image]
+    //             ]
+    //         }),
+    //         success: function (response) {
+    //             console.log(response);
+    //         },
+    //         error: function (err) {
+    //             console.log(err);
+    //         }
+    //     });
+    // });
+    var isPlaying = false;
+    var bgAudio = new Audio('./assets/audio/bgAudio.mp3');
+
+    $("#voice-control-btn").click(function () {
+        if (isPlaying) {
+            bgAudio.pause();
+            isPlaying = false;
+        } else {
+            bgAudio.play();
+            isPlaying = true;
+        }
+    })
 
 });
+
