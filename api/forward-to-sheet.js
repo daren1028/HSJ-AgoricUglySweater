3ccd3079e846b3072f7383f66b5ea440fa65ebd3
caddoSmith229 <caddo.caleb229@gmail.com>
2023-12-04 19:52:59 -0600

    fixed

--- api/forward-to-sheet.js
+++ api/forward-to-sheet.js
@@ -1,14 +1,19 @@
-import fetch from 'node-fetch';
-
 module.exports = async (req, res) => {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyTBSZele7wYHYVKqPivUYIq1o8qBW_DFIzE-nzVaBq8iBchKMJG6Wg3H8dkqHD0oM/exec', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(req.body)
-    });
+    const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-    const data = await response.text();
-    res.status(200).send(data);
+    try {
+        const response = await fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(req.body)
+        });
+
+        const data = await response.text();
+        res.status(200).send(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error in serverless function');
+    }
 };
